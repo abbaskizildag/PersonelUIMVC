b2c8fabc01db129f0b60baf5339e56eca346e579
abbaskizildag <58951278+abbaskizildag@users.noreply.github.com>
2020-10-18 19:17:56 +0300

    ajax ile actinlink

--- PersonelUI/Controllers/PersonelController.cs
+++ PersonelUI/Controllers/PersonelController.cs
@@ -73,5 +73,11 @@ namespace PersonelUI.Controllers
             db.SaveChanges();
             return RedirectToAction("Index");
         }
+
+        public ActionResult PersonelleriListele(int id)
+        {
+            var model = db.Personel.Where(x => x.DepartmanId == id).ToList();
+            return PartialView(model);
+        }
     }
 }
